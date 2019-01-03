f9d9ead2c846f56103ef4e1dfafd099a1ba6e724
Karunam Goyal <karunam.goyal25@gmail.com>
2019-01-03 18:09:40 +0530

    -page jumping corrected

---
+++ app/src/main/java/vision/karunamgoyal/vision/CounsellorActivity.java
@@ -0,0 +1,204 @@
+package vision.karunamgoyal.vision;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CounsellorActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private static String ausername;
+    private String str = "Message Checking";
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPageAdapter viewPageAdapter;
+    private FloatingActionButton fab;
+    String data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counsellor);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Intent in = getIntent();
+        ausername = in.getStringExtra(str);
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager=findViewById(R.id.viewPager);
+        viewPageAdapter=new ViewPageAdapter(getSupportFragmentManager());
+        //Add fragmentViewPageAdapter
+        viewPageAdapter.AddFragment(new ExamFragment(),"Exams");
+        viewPageAdapter.AddFragment(new InstituteFragment(),"Institutes");
+        viewPageAdapter.AddFragment(new StudyFragment(),"Study");
+        viewPager.setAdapter(viewPageAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        data="exam";
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addexam(data);
+            }
+        });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                switch (i){
+                    case 0:
+                        data="exam";
+                        break;
+                    case 1:
+                        data="insti";
+                        break;
+                    case 2:
+                        data="study";
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Intent in = getIntent();
+        ausername = in.getStringExtra(str);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent in = getIntent();
+        ausername = in.getStringExtra(str);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent in = getIntent();
+        ausername = in.getStringExtra(str);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.counsellor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent =new Intent(this,AppSettings.class);
+            intent.putExtra(str, ausername);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Intent intent =new Intent(this,CounsellorProfile.class);
+            intent.putExtra(str, ausername);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_slideshow) {
+
+            Intent intent =new Intent(this,Profile.class);
+            intent.putExtra(str, ausername);
+            startActivity(intent);
+        } else if (id == R.id.nav_manage) {
+            SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+            final SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+            editor.commit();
+            Intent in = new Intent(this, MainActivity.class);
+            startActivity(in);
+
+        }
+        else if(id==R.id.nav_discuss){
+            Intent intent =new Intent(this,DiscussActivity.class);
+            intent.putExtra(str, ausername);
+            startActivity(intent);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public void addexam(String data){
+        Intent intent;
+        if(data.equals("exam"))
+         intent=new Intent(this,AddExam.class);
+        else if(data.equals("insti")){
+
+            intent=new Intent(this,addInstitute.class);
+        }
+        else{
+            intent=new Intent(this,AddStudy.class);
+        }
+         intent.putExtra(str,ausername);
+        Log.v("helloletsee","177");
+        startActivity(intent);
+    }
+
+    public static String getMyData() {
+        return ausername;
+    }
+}
---
+++ app/src/main/java/vision/karunamgoyal/vision/addInstitute.java
@@ -0,0 +1,163 @@
+package vision.karunamgoyal.vision;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class addInstitute extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    TextView name;
+    TextView desc;
+    Spinner tag;
+    TextView url;
+    Button publish;
+    String name1;
+    String desc1;
+    String tag1;
+    String url1;
+    private String ausername;
+    private String str = "Message Checking";
+    private DatabaseReference mdatabadereference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_institute);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Intent in = getIntent();
+        ausername = in.getStringExtra(str);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mdatabadereference = FirebaseDatabase.getInstance().getReference().child("institute");
+        name = findViewById(R.id.editText2);
+        desc = findViewById(R.id.editText3);
+        url = findViewById(R.id.editText4);
+        tag = findViewById(R.id.spinner2);
+        publish = findViewById(R.id.buttonPublish);
+        publish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name1 = name.getText().toString();
+                desc1 = desc.getText().toString();
+                url1 = url.getText().toString();
+                tag1 = tag.getSelectedItem().toString();
+                Institutes study = new Institutes(name1, tag1, desc1, url1);
+                String key = mdatabadereference.push().getKey();
+                mdatabadereference.child(key).setValue(study);
+                Snackbar.make(v, "Successfully Published", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_institute, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, AppSettings.class);
+            intent.putExtra(str, ausername);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            SharedPreferences pref = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+            final SharedPreferences.Editor editor = pref.edit();
+            String userType = pref.getString("userType", "");
+            Intent in;
+            if (userType.equals("Student"))
+                in = new Intent(this, StudentActivity.class);
+            else
+                in = new Intent(this, CounsellorActivity.class);
+            in.putExtra(str, ausername);
+            startActivity(in);
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(this, CounsellorProfile.class);
+            intent.putExtra(str, ausername);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_slideshow) {
+
+            Intent intent = new Intent(this, Profile.class);
+            intent.putExtra(str, ausername);
+            startActivity(intent);
+        } else if (id == R.id.nav_manage) {
+            SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+            final SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+            editor.commit();
+            Intent in = new Intent(this, MainActivity.class);
+            startActivity(in);
+
+        } else if (id == R.id.nav_discuss) {
+            Intent intent = new Intent(this, DiscussActivity.class);
+            intent.putExtra(str, ausername);
+            startActivity(intent);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
